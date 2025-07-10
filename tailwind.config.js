1b083921232f1f43a09a39963dee4ccd753c9652
haclabs <36437587+haclabs@users.noreply.github.com>
2025-07-10 17:10:21 -0600

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx,css}'
   ],
   darkMode: 'class', // Enable class-based dark mode
   theme: {
