cbe4cae5a3325e187036640eb90bb2427a66aa30
haclabs <36437587+haclabs@users.noreply.github.com>
2025-07-12 22:40:53 -0600

    Updated package-lock.json

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,33 +1,18 @@
-@@ .. @@
- import type { StorybookConfig } from "@storybook/react-vite";
-+import { mergeConfig } from 'vite';
-+import path from 'path';
- 
- const config: StorybookConfig = {
-   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-@@ .. @@
-   framework: {
-     name: "@storybook/react-vite",
-     options: {},
-+  },
-+  viteFinal: async (config) => {
-+    return mergeConfig(config, {
-+      css: {
-+        postcss: {
-+          plugins: [
-+            require('postcss-import'),
-+            require('tailwindcss/nesting'),
-+            require('tailwindcss'),
-+            require('autoprefixer'),
-+          ],
-+        },
-+      },
-+      resolve: {
-+        alias: {
-+          '@': path.resolve(__dirname, '../src'),
-+        },
-+      },
-+    });
-   }
- };
- export default config;
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
